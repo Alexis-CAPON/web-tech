89b507202a1961ff4a5dd55c0bdd0ac996929fb1
Alexis <72978378+Alexis-CAPON@users.noreply.github.com>
2021-09-17 11:40:29 +0200

    feat : added handles.js; modified index.js, package.json

--- labs/01.nodejs/index.js
+++ labs/01.nodejs/index.js
@@ -1,14 +1,5 @@
-// Import a module
+// ./index.js
 const http = require('http')
-
-// Declare an http server
-http.createServer(function (req, res) {
-
-  // Write a response header
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  // Write a response content
-  res.end('Hello World Alexis\n');
-
-// Start the server
-}).listen(8080)
+const handles = require('./handles')
+const server = http.createServer(handles.serverHandle);
+server.listen(8080)
